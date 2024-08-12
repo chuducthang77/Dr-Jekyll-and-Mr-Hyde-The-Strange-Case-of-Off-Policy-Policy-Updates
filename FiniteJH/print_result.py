1a97ff90af22a199005366c4390cfa021f5847e5
Thang Chu <chuducthang77@gmail.com>
2024-08-11 23:24:44 -0400

    Finish finite update experiment

--- FiniteJH/print_result.py
+++ FiniteJH/print_result.py
@@ -96,7 +96,7 @@ else:
     # print('---------------------------')
 
     # Load the training file    # Load all 100 results to calculate the mean
-    dir = './expes/fig_new_exact/11_exp_final_check/'
+    dir = './expes/fig_new_sample/first_successful_exp/'
     result = {}
     for name in os.listdir(dir):
         if '.pkl' in name:
@@ -131,7 +131,7 @@ else:
         plt.plot(result[key], label=key)
     plt.legend()
     # plt.show() # Only uncomment this on the local machine
-    plt.savefig('fig6_11_exp_final_check.png')
+    plt.savefig('first_successful_exp_fit_new_sample.png')
 
     # # Save the result to transfer among machines
     # with open('result.pkl', 'wb') as fp:
